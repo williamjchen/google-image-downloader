330cbb6d37c1018e5134d1d926f39953bfa1836b
William <williamchenjian@gmail.com>
2020-07-03 17:53:06 -0400

    updated code and readme. added requirements.txt

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # Google-Image-Downloader
 Downloads bulk images from google. Useful for creating dataests
-
+- downloads original images
+- multithreaded for better performance
 ## Requirements
-- Selenium
+- selenium
+- os
+- requests
+- threading
+- time
+- argparse
 ## Usage
 ```
-downloader.py [-h] [-k search_term] [-N num__of_images]
+downloader.py [-h] [-k search_term] [-N num__of_images] [-t num_of_threads]
 ```
+- only the search term is a mandatory field
 
 ### Example
+- downloads first 10 images from the results of google image search of "dog". Uses max 15 threads
 ```
-downloader.py -k "dog" -N 10
+downloader.py -k "dog" -N 10 -t 15
 ```
-downloads first 10 images from the results of google image search of "dog"
+- downloads first 50 images from the results of google image search of "bear". Uses max 10 threads
+```
+downloader.py -k "bear"
+```
--- downloader.py
+++ downloader.py
@@ -9,22 +9,24 @@ counter = 0
 DRIVER_PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 
-def download_image(image_url, name):
-    global counter
+def download_image(image_url, name, thread_limiter):
+    thread_limiter.acquire()
     try:
-        r = requests.get(image_url)
-        with open(name, "wb") as f:
-            f.write(r.content)
-        print(f"Saved Image at:{name}, url:{image_url}")
-        counter += 1
-    except Exception as e:
-        print("ಠ_ಠ. Could not download image", e)
+        try:
+            r = requests.get(image_url)
+            with open(name, "wb") as f:
+                f.write(r.content)
+            print(f"Saved Image at:{name}, url:{image_url}\n")
+        except Exception as e:
+            print("ಠ_ಠ. Could not download image", e)
+    finally:
+        thread_limiter.release()
 
 
-def get_links(keyword, num_of_images=50):
+def get_links(keyword, num_of_images, maximum_threads):
     global DRIVER_PATH
     wd = webdriver.Chrome(DRIVER_PATH)
-
+    thread_limiter = threading.BoundedSemaphore(maximum_threads)
     url = f"https://www.google.com/search?q={keyword}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjQuNTC_pbqAhXtnuAKHVFaC84Q_AUoAXoECB4QAw&biw=1920&bih=975"
     global counter
 
@@ -49,30 +51,32 @@ def get_links(keyword, num_of_images=50):
                 image.click()
             except Exception as e:
                 continue
-            time.sleep(0.5)
-            print("made it")
+            time.sleep(0.6)
             actual_images = wd.find_elements_by_css_selector("img.n3VNCb")
-
             for actual_image in actual_images:
                 if "http" in actual_image.get_attribute("src"):
-                    download_image(actual_image.get_attribute("src"), os.path.join(path, f"{keyword}{counter+1}.png"))
+                    counter += 1
+                    t = threading.Thread(target=download_image, args=(actual_image.get_attribute("src"), os.path.join(path, f"{keyword}{counter}.png"), thread_limiter))
+                    t.start()
+                    if counter == num_of_images:
+                        break
+
             if counter == num_of_images:
                 break
-
     wd.quit()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download Images from Google for your dataset")
     parser.add_argument("-k", "--search_term", type=str, required=True, help="What you want images of")
-    parser.add_argument("-N", "--num_of_images", type=int, help="How many images you want")
+    parser.add_argument("-N", "--num_of_images", default=50, type=int, help="How many images you want")
+    parser.add_argument("-t", "--num_of_threads", default=10, type=int, help="maximum number of threads used by program")
     args = parser.parse_args()
 
     if not args.search_term:
         parser.error("Please provide a search term")
     else:
         start = time.time()
-        print("hello")
-        get_links(args.search_term, args.num_of_images)
+        get_links(args.search_term, args.num_of_images, args.num_of_threads)
         end = time.time()
         print(f"{args.num_of_images} images downloaded in {end - start} seconds.")
